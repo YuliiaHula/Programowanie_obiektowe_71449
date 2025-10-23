715a14413d4acc73531f3b5c4c4c883973a26424
YuliiaHula <yuliiahula12@gmail.com>
2025-10-23 21:11:40 +0200

    dodano punkt 18

---
+++ Lab1/Program_L1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("To jest cwiczenie 1");
+        }
+    }
+}
