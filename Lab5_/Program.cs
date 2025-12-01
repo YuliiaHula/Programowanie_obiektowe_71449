946b47e353d0c6de8259345a5af6885c5530981c
Yuliia Hula <71449@student.wsiz.edu.pl>
2025-12-01 14:40:50 +0100

    Lab5 zadanie 1

---
+++ Lab5_/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Lab5_
+{
+    class Program
+    {
+        public static void Main()
+        {
+        }
+
+        //zadanie1
+        public static void zadanie1(string nazwaPliku = "dane.txt", int ileRazy = 3)
+        {
+            try
+            {
+                Console.WriteLine($"Podaj {ileRazy} informacji:");
+
+                using (StreamWriter writer = new StreamWriter(nazwaPliku))
+                {
+                    for (int i = 0; i < ileRazy; i++)
+                    {
+                        Console.Write($"Informacja {i + 1}: ");
+                        string tekst = Console.ReadLine();
+                        writer.WriteLine(tekst);
+                    }
+                }
+
+                Console.WriteLine($"\nDane zostały zapisane do pliku: {nazwaPliku}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Błąd podczas zapisu: {ex.Message}");
+            }
+        }
+    }
+}
